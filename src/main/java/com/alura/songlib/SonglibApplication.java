f068ae46c63b4ad0d6e6e57b7b097233d12d2a85
luk3mn <lucasrenan2015@gmail.com>
2024-05-09 10:14:52 -0300

    init: project initializing

---
+++ src/main/java/com/alura/songlib/SonglibApplication.java
@@ -0,0 +1,13 @@
+package com.alura.songlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonglibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonglibApplication.class, args);
+	}
+
+}
